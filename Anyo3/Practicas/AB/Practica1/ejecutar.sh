f049d5d4747b709baddf4940c7936add5d995f57
GryzleeVEVO <dorian.wozniak@gmail.com>
2023-03-07 14:59:06 +0100

    07/03/23

--- Anyo3/Practicas/AB/Practica1/ejecutar.sh
+++ Anyo3/Practicas/AB/Practica1/ejecutar.sh
@@ -32,49 +32,49 @@ test_precond() {
     ./MHcypher -e 3 8 13 28 60 124 256 493 1001 977 &>/dev/null </dev/null && bien || mal
 }
 
-test_encriptar() {
+test_encriptar_ascii() {
     echo -ne "Test 1: Texto genérico (lorem ipsum) "
-    ./MHcypher -e -f test1 -o cifrado $@
+    ./MHcypher -e -f ./test/test1 -o cifrado $@
     ./MHcypher -d -f cifrado -o descifrado $@
-    cmp test1 descifrado &>/dev/null </dev/null && bien || mal
+    cmp ./test/test1 descifrado &>/dev/null </dev/null && bien || mal
     echo -ne "Test 2: 20KB de datos aleatorios "
-    ./MHcypher -e -f test2 -o cifrado $@
+    ./MHcypher -e -f ./test/test2 -o cifrado $@
     ./MHcypher -d -f cifrado -o descifrado $@
-    cmp test2 descifrado &>/dev/null </dev/null && bien || mal
+    cmp ./test/test2 descifrado &>/dev/null </dev/null && bien || mal
     echo -ne "Test 3: MHcypher.cc, utilizando codificación ASCII "
-    ./MHcypher -e -f test3 -o cifrado $@
+    ./MHcypher -e -f ./test/test3 -o cifrado $@
     ./MHcypher -d -f cifrado -o descifrado $@
-    cmp test3 descifrado &>/dev/null </dev/null && bien || mal
+    cmp ./test/test3 descifrado &>/dev/null </dev/null && bien || mal
 }
 
-test_encriptar_ascii() {
+test_encriptar_mayus() {
     echo -ne "Test 4: Ejemplo del enunciado (codif. mayúsculas) "
-    ./MHcypher -mayus -e -f test4 -o cifrado $@
+    ./MHcypher -mayus -e -f ./test/test4 -o cifrado $@
     ./MHcypher -mayus -d -f cifrado -o descifrado $@
-    cmp test4 descifrado &>/dev/null </dev/null && bien || mal
+    cmp ./test/test4 descifrado &>/dev/null </dev/null && bien || mal
     echo -ne "Test 5: 20KB de mayusculas aleatorias (codif. mayúsculas) "
-    ./MHcypher -mayus -e -f test5 -o cifrado $@
+    ./MHcypher -mayus -e -f ./test/test5 -o cifrado $@
     ./MHcypher -mayus -d -f cifrado -o descifrado $@
-    cmp test5 descifrado &>/dev/null </dev/null && bien || mal
+    cmp ./test/test5 descifrado &>/dev/null </dev/null && bien || mal
 }
 
 # Compila, en caso de usar hendrix01 o 02 usa gmake
-[ $(uname) -eq "SunOS" ] && gmake || make
+[ $(uname) = "SunOS" ] && gmake || make
 
 test_precond
 
 echo -e "\nClave privada 1: [3 8 13 28 60 124 256 493], N = 1001, w = 977"
-test_encriptar 3 8 13 28 60 124 256 493 1001 977
 test_encriptar_ascii 3 8 13 28 60 124 256 493 1001 977
+test_encriptar_mayus 3 8 13 28 60 124 256 493 1001 977
 
 echo -e " \nClave privada 2: [5 7 28 46 90 180 380 740 1480], N = 1820, w = 1431"
-test_encriptar 5 7 28 46 90 180 380 740 1480 3410 2797
 test_encriptar_ascii 5 7 28 46 90 180 380 740 1480 3410 2797
+test_encriptar_mayus 5 7 28 46 90 180 380 740 1480 3410 2797
 
 echo -e "\nClave privada 3: [2 3 7 15 31], N = 61, w = 17"
-test_encriptar_ascii 2 3 7 15 31 61 17
+test_encriptar_mayus 2 3 7 15 31 61 17
 
 echo -e "\nClave privada 4: [3, 4, 10, 18, 40, 83] N = 224, w = 179"
-test_encriptar_ascii 3 4 10 18 40 83 224 179
+test_encriptar_mayus 3 4 10 18 40 83 224 179
 
 rm cifrado descifrado
--- Anyo3/Practicas/PLeng/Practica1/doc/README.txt
+++ Anyo3/Practicas/PLeng/Practica1/doc/README.txt
@@ -1,6 +1,8 @@
-Compilador clike.jar (V1.0)
+Compilador clike.jar
 ------------------------------
 Análisis léxico y sintáctico
+
+Autor: Wozniak, Dorian Boleslaw  (817570@unizar.es)
 
 
 Invocar como:
@@ -11,9 +13,5 @@ java -jar clike.jar <fichero_fuente_clike>
 
 Si se invoca sin parámetros, lee la entrada estándar.
 
-
-Características generales:
-1) No permite funciones anidadas (se ha comentado la parte que lo permite)
-2) Usa LOOKAHEAD, aunque es fácilmente transformable en LL(1)
-3) En el nivel más alto, se pueden intercalar declaraciones de funciones/procedimientos y declaraciones de variables. No así dentro de una función (ya que no hay anidamiento)
-4) No se pueden intercalar instrucciones con declaraciones dentro de una func/proc: primero las declaraciones de variables, luego las instrucciones
+El programa se ha probado con la batería de programas de test dada y todas
+son analizadas sin errores. 
