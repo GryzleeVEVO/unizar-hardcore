fb90df265f5f93d72754fd0dc3328ef229f91821
GryzleeVEVO <dorian.wozniak@gmail.com>
2023-03-08 23:55:28 +0100

    08/03/23

--- Anyo3/Practicas/AprAuto/Practica3/CosteLogistico.m
+++ Anyo3/Practicas/AprAuto/Practica3/CosteLogistico.m
@@ -1,21 +1,15 @@
 function [J, grad, Hess] = CosteLogistico(theta, X, y)
-    N = size(X,2);
+    % Calcula el coste logistico, y si se piden,
+    % su gradiente y su Hessiano
+    N = size(X, 2);
+    h = 1 ./ (1 + exp(- (X * theta)));
+    J = (-y' * log(h) - (1 - y') * log(1 - h)) / N;
 
-    % Hipótesis
-    h = 1 ./ (1 + exp(-X* theta)); 
-
-    % Función coste (máxima verisimilitud)
-    J = (-y' * log(h) - (1-y') * log(1-h))/N; 
-    
     if nargout > 1
-        % Gradiente
-        grad = X' * (h-y) / N;
+        grad = X' * (h - y) / N;
     end
 
     if nargout > 2
-        % Hessiano
-        R = diag(h.*(1-h));
-        Hess = X' * R*X/N;
+        R = diag(h .* (1 - h));
+        Hess = X' * R * X / N;
     end
-end
-
